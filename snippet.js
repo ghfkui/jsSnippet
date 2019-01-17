8663a8036105744b51d1a65ddd2a1c8bf6bb944c
deanfeng <dean.feng@synchronoss.com>
2019-01-17 11:17:37 +0800

    isArray,flatArr,arrToObj

---
+++ snippet.js
@@ -0,0 +1,24 @@
+function isArray(arr) {
+  return Object.prototype.toString.call(arr) === '[object Array]'
+  // return arr instanceof Array
+  // return arr.constructor === Array
+}
+
+// flat multidimensional arrays
+function flatArr(arr) {
+  if (isArray(arr)) {
+    return arr.flatMap(a => {
+      if (isArray(arr)) {
+        return flatArr(a)
+      }
+      return a
+    })
+  }
+  return arr
+}
+
+function arrToObj(arr) {
+  return Object.assign({}, arr)
+  // return {...arr}
+}
+
